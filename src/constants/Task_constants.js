b35ac913584c76e3f5246e56e7a7a9c7548877ea
jemeetala <jemee.tala@dhiwise.com>
2022-06-27 08:20:45 +0000

    Code Synced by DhiWise

--- src/constants/Task_constants.js
+++ src/constants/Task_constants.js
@@ -1,5 +1,5 @@
 export const consttask = {
-  owner_id: 70000078627,
-  outcome_id: 70002087555,
-  task_type_id: 70000179274,
+  owner_id: "70000078627",
+  outcome_id: "70002087555",
+  task_type_id: "70000179274",
 };
